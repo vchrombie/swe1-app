8402df9d02987471056f3e80995cf83d2acbab05
Venu Vardhan Reddy Tekula <vt2182@nyu.edu>
2023-02-20 14:10:51 -0500

    [swe1-app] use python-decouple for storing secret keys

--- swe1_app/settings.py
+++ swe1_app/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1lcoq)f(i880w+73gom4kie7v8nu!dpn+^_1_mfxl$c*1bg$7#'
+
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
