63c1b5a1eef36815535775261f4eb8fce79db472
Venu Vardhan Reddy Tekula <vt2182@nyu.edu>
2023-02-20 13:07:45 -0500

    [polls] handling 404 error in detail view

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 
 from .models import Question
@@ -11,8 +11,8 @@ def index(request):
 
 
 def detail(request, question_id):
-    response = "You're looking at question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
